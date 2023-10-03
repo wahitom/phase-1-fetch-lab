f898f6c8f0e8c6dd110562183271636f01610b4a
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-10-04 01:10:17 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+  .then(resp => resp.json())
+  .then(books => renderBooks(books))  
 }
 
 function renderBooks(books) {
@@ -10,8 +12,10 @@ function renderBooks(books) {
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
+
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
